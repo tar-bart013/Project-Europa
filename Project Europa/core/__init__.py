e817f38b8823b7875fc10d332bc27b45f0b231ea
Tar Bart <160907673+tar-bart013@users.noreply.github.com>
2024-04-02 13:21:14 +0300

    Update __init__.py

--- Project Europa/core/__init__.py
+++ Project Europa/core/__init__.py
@@ -5,7 +5,7 @@
 __all__ = ['database','generators','handler','loader','payloads','security','stagers','util']
 __version__ = '0.1'
 __license__ = 'GPLv3'
-__github__ = 'https://github.com/tarbart013/Project-Europa
+__github__ = 'https://github.com/tar-bart013/Project-Europa
 
 # def main():
 #     for module in __all__:
